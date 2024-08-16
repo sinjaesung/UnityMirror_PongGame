ec21ebe968d456fde502c12825a5e06bce2e102a
sinjaesung <gotodongyang@naver.com>
2024-08-16 17:04:27 +0900

    퐁게임 커밋>
    퐁게임 커밋>

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    public float speed = 30;
+    public Rigidbody2D rigidbody2d;
+
+    // need to use FixedUpdate for rigidbody
+    void FixedUpdate()
+    {
+        // only let the local player control the racket.
+        // don't control other player's rackets
+        if (isLocalPlayer)
+            rigidbody2d.velocity = new Vector2(0, Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
+    }
+}
